4eda9ccdad6d7f164d2259dab2cd24ee1900ef80
feiyu <lvp123321123321@gmail.com>
2025-11-24 13:48:13 +0700

    fix potential build error in some distros

--- src/scripts/general-setup.sh
+++ src/scripts/general-setup.sh
@@ -4,7 +4,7 @@
 
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y build-essential clang cmake nasm
-sudo apt install -y libc++-dev libc++abi-dev libjsoncpp-dev uuid-dev zlib1g-dev zip unzip screen
+sudo apt install -y libc++-dev libc++abi-dev libjsoncpp-dev uuid-dev zlib1g-dev zip unzip screen g++ libstdc++-12-dev
 
 # Bob
 

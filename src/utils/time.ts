8198aec794cd5cae70838cc609c1734aeea952be
feiyu <lvp123321123321@gmail.com>
2026-01-28 20:25:14 +0700

    fix get last wed bug

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -13,9 +13,13 @@ export function getLastWednesdayTimestamp() {
     const dayOfWeek = now.getUTCDay();
 
     // Calculate how many days ago the last Wednesday was
-    // If today is Wednesday, this will return 7 (to get the *previous* one)
-    // If you want today's date when today is Wednesday, change (dayOfWeek + 3) % 7 || 7 to (dayOfWeek + 4) % 7
-    const daysSinceLastWednesday = (dayOfWeek + 4) % 7;
+    // If today is Wednesday, this will return 7 (to get the *previous* one) if current time is past 12:00 UTC
+    // or 0 (to get today) if current time is before 12:00 UTC
+    let daysSinceLastWednesday = (dayOfWeek + 4) % 7;
+    if (daysSinceLastWednesday === 0 && now.getUTCHours() < 12) {
+        // If today is Wednesday and before 12:00 UTC, go back 7 days to get the previous Wednesday
+        daysSinceLastWednesday = 7;
+    }
 
     // Create a new date object for that day
     const lastWednesday = new Date(now);
